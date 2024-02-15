414df779b6dfc909c3a21236e1573324aa6b51c0
XxThunderBlastxX <koustavmondal55@gmail.com>
2024-02-15 23:46:30 +0530

    Added a loader to contact card

--- components/ui/contact-form.tsx
+++ components/ui/contact-form.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import React, { useState } from "react";
+import axios from "axios";
+import { ClipLoader } from "react-spinners";
 
 function ContactForm() {
   const [contactForm, setContactForm] = useState({
@@ -9,8 +11,33 @@ function ContactForm() {
     message: "",
   });
 
+  const [isLoading, setIsLoading] = useState(false);
+
   async function submitContactForm() {
-    console.log(contactForm.email, contactForm.name, contactForm.message);
+    setIsLoading(true);
+
+    const data: { name: string; email: string; message: string } = {
+      name: contactForm.name.trim(),
+      email: contactForm.email.trim(),
+      message: contactForm.message.trim(),
+    };
+
+    await axios
+      .post("https://porfolio-api.koustav.dev/send-message", {
+        data,
+      })
+      .then((res) => {
+        if (res.status === 200) {
+          setIsLoading(false);
+          alert("Message sent successfully");
+          return;
+        }
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        alert("Message sending failed" + err);
+        return;
+      });
   }
 
   return (
@@ -22,6 +49,7 @@ function ContactForm() {
           required={true}
           name={"name"}
           value={contactForm.name}
+          placeholder={"Enter your name"}
           onChange={(e) =>
             setContactForm({ ...contactForm, name: e.target.value })
           }
@@ -35,6 +63,7 @@ function ContactForm() {
           required={true}
           name={"email"}
           value={contactForm.email}
+          placeholder={"Enter your email"}
           onChange={(e) =>
             setContactForm({ ...contactForm, email: e.target.value })
           }
@@ -50,18 +79,19 @@ function ContactForm() {
           onChange={(e) =>
             setContactForm({ ...contactForm, message: e.target.value })
           }
+          placeholder={"Enter your message"}
           className={
             "h-20 w-full rounded-md border-[1px] border-white/[0.45] bg-transparent px-2 py-2 text-white hover:border-white/[0.75] focus:border-white/[0.75]"
           }
         />
         <button
           type="submit"
-          onClick={() => submitContactForm}
+          onClick={() => submitContactForm()}
           className={
             "mt-4 h-10 w-full rounded-md bg-white text-black hover:border-[1px] hover:border-white hover:bg-black hover:bg-transparent hover:text-white"
           }
         >
-          Submit
+          {isLoading ? <ClipLoader color={"white"} size={26} /> : "Submit"}
         </button>
       </form>
     </>
