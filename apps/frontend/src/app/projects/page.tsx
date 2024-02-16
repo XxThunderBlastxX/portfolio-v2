ee12a14659e42da5055d95bd7d394a95b56528c1
XxThunderBlastxX <koustavmondal55@gmail.com>
2024-02-16 20:56:25 +0530

    Added meta tags

--- apps/frontend/src/app/projects/page.tsx
+++ apps/frontend/src/app/projects/page.tsx
@@ -1,53 +1,7 @@
-"use client";
-
 import React from "react";
 import {HoverEffect} from "../../../components/ui/card-hover-background-effect";
 import BackButton from "../../../components/ui/back-button";
 import projectConfig from "../../../config/projects.config";
-
-const projects = [
-    {
-        title: "Flutter Dependency Adder",
-        description:
-            "It is a platform to add all your favourite dependencies using a single command.",
-        stack: ["Flutter", "Dart", "Netlify"],
-        link: "https://github.com/XxThunderBlastxX/flutter_dependency_adder",
-    },
-    {
-        title: "Taskbit",
-        description:
-            "An android application to streamline task management and enhance your productivity.",
-        stack: ["Flutter", "Dart", "Appwrite", "Riverpod"],
-        link: "https://github.com/XxThunderBlastxX/taskbit",
-    },
-    {
-        title: "Scribe",
-        description:
-            "A mobile application for taking short notes on your mobile phone.",
-        stack: [
-            "Flutter",
-            "Dart",
-            "Riverpod",
-            "Firebase cloud firestore",
-            "Firebase authentication",
-        ],
-        link: "https://github.com/XxThunderBlastxX/scribe",
-    },
-    {
-        title: "Chatting-API",
-        description:
-            "Websocket based chat application backend written in Go and Pub/Sub(without any package). It uses Redis for caching and MongoDB for storing messages.",
-        stack: ["Go", "Redis", "GoFiber", "MongoDB"],
-        link: "https://github.com/XxThunderBlastxX/chamting-api",
-    },
-    {
-        title: "Aioli",
-        description:
-            "It is a CLI based text chat bot to answer your questions powered with Gemini-Pro.",
-        stack: ["Go", "urfave/cli", "Gemini-Pro"],
-        link: "https://github.com/XxThunderBlastxX/chamting-api",
-    },
-];
 
 function Projects() {
     return (
