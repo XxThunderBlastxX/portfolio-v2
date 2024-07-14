cdc0e3e08ce0508db2d3f65d163820c47daf1368
Koustav Mondal <koustavmondal55@gmail.com>
2024-07-14 18:17:03 +0000

    Fixed caching issue for server action

--- src/components/actions/projects_action.ts
+++ src/components/actions/projects_action.ts
@@ -1,57 +1,62 @@
 "use server";
 
 export interface Project {
-    name: string;
-    description: string;
-    stacks: string[];
-    link: string;
+  name: string;
+  description: string;
+  stacks: string[];
+  link: string;
 }
 
 interface ProjectApiResponse {
-    data: {
-        projects: {
-            ID: number;
-            name: string;
-            description: string;
-            link: string;
-            stacks: string[];
-            CreatedAt: string;
-            UpdatedAt: {
-                Time: string;
-                Valid: boolean;
-            };
-        }[];
-    };
-    success: boolean;
+  data: {
+    projects: {
+      ID: number;
+      name: string;
+      description: string;
+      link: string;
+      stacks: string[];
+      CreatedAt: string;
+      UpdatedAt: {
+        Time: string;
+        Valid: boolean;
+      };
+    }[];
+  };
+  success: boolean;
 }
 
 function parseProjects(response: string): Project[] {
-    const parsedData: ProjectApiResponse = JSON.parse(response) as ProjectApiResponse;
-    if (!parsedData.success) {
-        throw new Error('API request failed');
-    }
+  const parsedData: ProjectApiResponse = JSON.parse(
+    response,
+  ) as ProjectApiResponse;
+  if (!parsedData.success) {
+    throw new Error("API request failed");
+  }
 
-    return parsedData.data.projects.map((project) => ({
-        name: project.name,
-        description: project.description,
-        stacks: project.stacks,
-        link: project.link,
-    }));
+  return parsedData.data.projects.map((project) => ({
+    name: project.name,
+    description: project.description,
+    stacks: project.stacks,
+    link: project.link,
+  }));
 }
 
-
 export default async function getProjects(): Promise<Project[]> {
-    try {
-        const res = await fetch("https://api.koustav.dev/projects/list")
+  try {
+    const res = await fetch("https://api.koustav.dev/projects/list", {
+      next: {
+        revalidate: 60,
+      },
+    });
 
-        if (res.ok && res.status === 200) {
-            const data = await res.text();
-            return parseProjects(data).reverse();
-        }
-
-        return []
-    } catch (err) {
-        console.error(err);
-        return [];
+    if (res.ok && res.status === 200) {
+      const data = await res.text();
+      return parseProjects(data).reverse();
     }
+
+    return [];
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
 }
